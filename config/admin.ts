69f349d7391c6ad4e1d4ec0f4f13c7e70d52e0e1
premkumar-softscripts <prem@softscripts.net>
2025-02-07 18:48:29 +0530

    third

--- config/admin.ts
+++ config/admin.ts
@@ -3,7 +3,7 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', "mnaXHvsMpEKa339MdrJpQQ=="),
   },
   transfer: {
     token: {
